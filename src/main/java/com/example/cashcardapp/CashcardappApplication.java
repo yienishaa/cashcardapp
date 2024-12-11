95c2affd8aca43509673fa4e7eca5e560575b27f
Yienisha Abeyratne <yienishaedu@gmail.com>
2024-12-10 22:36:26 -0500

    All files

---
+++ src/main/java/com/example/cashcardapp/CashcardappApplication.java
@@ -0,0 +1,13 @@
+package com.example.cashcardapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashcardappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashcardappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cashcardapp/CashcardappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cashcardapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashcardappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
